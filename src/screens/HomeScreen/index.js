01eeb9ae6894018136dca92012e6928025d467c1
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-25 18:05:58 +0530

    :sparkles: displays home screen of the app

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,24 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import React from "react";
+import RestaurantItem from "../../components/RestaurantItem";
+import restaurants from "../../../assets/data/restaurants.json";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10
+  },
+});
