20876c869b707c46895ac906c48559849004539d
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-25 18:07:59 +0530

    :sparkles: displays the restaurant name and brand image

---
+++ src/screens/RestaurantDetailsScreen/Header.js
@@ -0,0 +1,24 @@
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import restaurants from "../../../assets/data/restaurants.json";
+import DishListItem from "../../components/DishListItem";
+import styles from "./styles";
+
+const RestaurantHeader = ({ restaurant }) => {
+  return (
+    <View style={styles.page}>
+      <Image source={{ uri: restaurant.image }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{restaurant.name}</Text>
+        <Text style={styles.subtitle}>
+          $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+          {restaurant.maxDeliveryTime} minutes
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default RestaurantHeader;
