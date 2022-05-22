f5b8519339eeba667455eccf6e72f78cf80abe77
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-22 17:15:01 +0530

    :zap: added Root navigator to Navigation container

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import HomeScreen from "./src/screens/HomeScreen";
-import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './src/navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <HomeScreen /> */}
-      <RestaurantDetailsScreen />
-
-      <StatusBar style="light" />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    // padding: 10,
-    // paddingVertical: 30
-  },
-});
