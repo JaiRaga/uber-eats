2dfb06312c98d4cedee889f24bdd361f0d3f11a2
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-22 17:13:35 +0530

    :recycle: refactored code

---
+++ src/screens/OrdersScreen/OrdersScreen.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import OrderListItem from '../../components/OrderListItem/OrderListItem';
+import orders from '../../../assets/data/orders.json';
+
+const OrdersScreen = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={orders}
+        renderItem={({ item }) => <OrderListItem order={item} />}
+      />
+    </View>
+  );
+};
+
+export default OrdersScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingTop: 50,
+  },
+});
