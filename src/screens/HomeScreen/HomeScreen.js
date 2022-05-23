1d2f96b2e858b8494852af3aee896bb6bb659451
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-23 19:12:50 +0530

    :sparkles: queried the aws backend to get restaurants data from the datastore

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,9 +1,24 @@
 import { View, FlatList, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import RestaurantItem from '../../components/RestaurantItem';
-import restaurants from '../../../assets/data/restaurants.json';
+import { DataStore } from 'aws-amplify';
+import { Restaurant } from '../../models';
+
+// import restaurants from '../../../assets/data/restaurants.json';
 
 const HomeScreen = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  // const fetchRestaurants = async () => {
+  //   const results = await DataStore.query(Restaurant)
+  //   setRestaurants(results)
+  // }
+
+  useEffect(() => {
+    DataStore.query(Restaurant).then(setRestaurants);
+    // fetchRestaurants()
+  }, []);
+
   return (
     <View style={styles.page}>
       <FlatList
