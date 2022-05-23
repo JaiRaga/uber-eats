3d0e107a56dffadf532b816f505d0c2263c26858
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-23 19:10:19 +0530

    :sparkles: queried the aws backend to get details about the restaurant and the dishes avaliable using aws datastore

--- src/screens/RestaurantDetailsScreen/RestaurantDetailsScreen.js
+++ src/screens/RestaurantDetailsScreen/RestaurantDetailsScreen.js
@@ -1,28 +1,57 @@
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { useRoute, useNavigation } from '@react-navigation/native';
-
-import restaurants from '../../../assets/data/restaurants.json';
+import { DataStore } from 'aws-amplify';
+import { Restaurant, Dish } from '../../models';
+// Components
 import Header from './Header';
 import DishListItem from '../../components/DishListItem';
-
+// Styles
 import styles from './styles';
 
-const restaurant = restaurants[0];
-
 const RestaurantDetailsScreen = () => {
+  const [restaurant, setRestaurant] = useState(null);
+  const [dishes, setDishes] = useState([]);
+
   const route = useRoute();
   const navigation = useNavigation();
 
   const id = route.params.id;
-  // console.warn(id)
+
+  useEffect(() => {
+    // fetch restaurant with the id
+    DataStore.query(Restaurant, id).then(setRestaurant);
+    // fetch the dishes with restaurant id equal to id received as params
+    DataStore.query(Dish, (dish) => dish.restaurantID('eq', id)).then(
+      setDishes
+    );
+  }, []);
+
+  console.log(restaurant);
+
+  if (!restaurant) {
+    return (
+      <ActivityIndicator
+        size={'large'}
+        color="black"
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      />
+    );
+  }
 
   return (
     <View style={styles.page}>
       <FlatList
         ListHeaderComponent={() => <Header restaurant={restaurant} />}
-        data={restaurant.dishes}
+        data={dishes}
         renderItem={({ item }) => <DishListItem dish={item} />}
         keyExtractor={(item, index) => index}
       />
