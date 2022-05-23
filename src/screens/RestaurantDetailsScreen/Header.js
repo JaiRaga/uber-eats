c5a289a9f39deed90f128376b5c4031575ef840f
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-23 19:09:12 +0530

    :sparkles: added a default image

--- src/screens/RestaurantDetailsScreen/Header.js
+++ src/screens/RestaurantDetailsScreen/Header.js
@@ -1,19 +1,29 @@
-import { View, Text, Image, FlatList, StyleSheet } from "react-native";
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
-import restaurants from "../../../assets/data/restaurants.json";
-import DishListItem from "../../components/DishListItem";
-import styles from "./styles";
+import restaurants from '../../../assets/data/restaurants.json';
+import DishListItem from '../../components/DishListItem';
+import styles from './styles';
+
+const DEFAULT_IMAGE =
+  'https://images.pexels.com/photos/2365940/pexels-photo-2365940.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
 const RestaurantHeader = ({ restaurant }) => {
   return (
     <View style={styles.page}>
-      <Image source={{ uri: restaurant.image }} style={styles.image} />
+      <Image
+        source={{
+          uri: restaurant.image.startsWith('http')
+            ? restaurant.image
+            : DEFAULT_IMAGE,
+        }}
+        style={styles.image}
+      />
       <View style={styles.textContainer}>
         <Text style={styles.title}>{restaurant.name}</Text>
         <Text style={styles.subtitle}>
-          $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+          $ {restaurant.deliveryFee.toFixed(1)} &#8226; {restaurant.minDeliveryTime}-
           {restaurant.maxDeliveryTime} minutes
         </Text>
         <Text style={styles.menuTitle}>Menu</Text>
