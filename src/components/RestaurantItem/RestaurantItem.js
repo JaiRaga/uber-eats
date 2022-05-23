0cca986d6e931b1e269aa799ed7955a90ec342b2
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-23 19:16:06 +0530

    :sparkles: added default image, fixed precision issue for ratings and delivery fee

--- src/components/RestaurantItem/RestaurantItem.js
+++ src/components/RestaurantItem/RestaurantItem.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
+const DEFAULT_IMAGE =
+  'https://images.pexels.com/photos/2365940/pexels-photo-2365940.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+
 const RestaurantItem = ({ restaurant }) => {
   const navigation = useNavigation();
   const onPress = () => {
@@ -12,7 +15,9 @@ const RestaurantItem = ({ restaurant }) => {
     <Pressable onPress={onPress} style={styles.restaurantContainer}>
       <Image
         source={{
-          uri: restaurant.image,
+          uri: restaurant.image.startsWith('http')
+            ? restaurant.image
+            : DEFAULT_IMAGE,
         }}
         style={styles.image}
       />
@@ -20,12 +25,12 @@ const RestaurantItem = ({ restaurant }) => {
         <View>
           <Text style={styles.title}>{restaurant.name}</Text>
           <Text style={styles.subtitle}>
-            $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+            $ {restaurant.deliveryFee.toFixed(2)} &#8226; {restaurant.minDeliveryTime}-
             {restaurant.maxDeliveryTime} minutes
           </Text>
         </View>
         <View style={styles.rating}>
-          <Text>{restaurant.rating}</Text>
+          <Text>{restaurant.rating.toFixed(1)}</Text>
         </View>
       </View>
     </Pressable>
