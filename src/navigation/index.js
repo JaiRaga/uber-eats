44c2722be589e853ae757455bd8c0c083532134f
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-24 19:49:55 +0530

    :sparkles: add check to display profile screen to get the user info or the restaurant screen to get the user order

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -10,13 +10,19 @@ import OrderDetailsScreen from '../screens/OrderDetailsScreen';
 import OrdersScreen from '../screens/OrdersScreen';
 import RestaurantDetailsScreen from '../screens/RestaurantDetailsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import { useAuthContext } from '../context/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
+  const { dbUser } = useAuthContext();
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="HomeTabs" component={HomeTabs} />
+      {dbUser ? (
+        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+      ) : (
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      )}
     </Stack.Navigator>
   );
 };
